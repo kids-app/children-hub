172719e217e8885496cdc747b29147cf5c40a69f
MsDiala <diala-is-diala@hotmail.com>
2021-02-03 01:18:24 +0200

    diala

--- react/blogapi/src/components/songs.js
+++ react/blogapi/src/components/songs.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import ReactPlayer from 'react-player';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles,withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import BookMarkIcon from '@material-ui/icons/Bookmark';
@@ -14,6 +14,9 @@ import FastForwardIcon from '@material-ui/icons/FastForward';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import Slider from '@material-ui/core/Slider';
+import Tooltip from '@material-ui/core/Tooltip'; 
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import FullScreenIcon from '@material-ui/icons/Fullscreen'; 
 
 
 const useStyles = makeStyles({
@@ -42,11 +45,60 @@ const useStyles = makeStyles({
         "&:hover":{
             color:'#fff',
             transform:"scale(1)",
-        }
-    }
+        },
+    },
+
+
+    bottomIcons:{
+        color:"#999",
+        "&:hover":{
+            color:"#fff",
+        },
+    },
+
+    volumeSlider :{
+        width:100,
+
+    },
 });
-
-
+function ValueLabelComponent(props) {
+    const { children, open, value } = props;
+  
+    return (
+      <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+        {children}
+      </Tooltip>
+    );
+  }
+const PrettoSlider = withStyles({
+    root: {
+      color:'pink',
+      height: 8,
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus, &:hover, &$active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: 'calc(-50% + 4px)',
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+    },
+    rail: {
+      height: 8,
+      borderRadius: 4,
+    },
+  })(Slider);
 function Songs() {
     const classes = useStyles();
 
@@ -120,6 +172,46 @@ function Songs() {
 
                                 {/*bottom controls */}
                                 <Grid container direction="row" justify="space-between" alignItems="center" style={{padding:16}}>
+                                    <Grid item xs={12}>
+                                        <PrettoSlider
+                                        min={0}
+                                        max={100}
+                                        defaultValue={20}
+                                        ValueLabelComponent={ValueLabelComponent}
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Grid container alignItems="center" direction="row">
+                                            <IconButton className={classes.bottomIcons}>
+                                                <PlayArrowIcon fontSize="large"/>
+                                            </IconButton>
+
+                                            <IconButton className={classes.bottomIcons}>
+                                                <VolumeUpIcon fontSize="large"/>
+                                            </IconButton>
+
+                                            <slider
+                                            min={0}
+                                            max={100}
+                                            defaultValue={100}
+                                            className={classes.volumeSlider}
+                                            />
+                                            <Button
+                                             variant="text"
+                                             style={{color:"#fff",marginLeft:16}}
+                                            >
+                                                <Typography>05:05</Typography>
+                                                
+                                            </Button>
+                                        </Grid>
+
+                                        </Grid>
+                                        <Grid item>
+                                            <Button variant="text" className={classes.bottomIcons}> 
+                                                <Typography>1X</Typography>
+                                            </Button>
+                                            <IconButton></IconButton>
+                                    </Grid>
 
 
                                 </Grid>
