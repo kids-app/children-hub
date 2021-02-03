ada0dbdc549ad5fa25bd8a2e13b30ecff67c375b
sondos-braim <sondos9811@gmail.com>
2021-02-03 20:24:13 +0200

    videos

--- react/blogapi/src/Videoid.js
+++ react/blogapi/src/Videoid.js
@@ -1,22 +1,27 @@
 import React, { useState, useRef, useEffect } from "react";
-import App from './App';
-
+import Youtube from './Youtubek';
+import "./css/youtube.css";
 function Videoid({match}){
   
     console.log(match.params.id)
         return(
-          <div>
+            <>
+          <div className='mainVid'>
           <Sss id={match.params.id} />
-          <App/>
-    </div>
-     
+          </div>
+          <div className='allVid'>
+          <Youtube/>
+          </div>
+         
+   
+     </>
         )
       }
 function Sss(props){
     return(
     <>
     
-    <iframe width="500" height="200" src={"https://www.youtube.com/embed/"+props.id} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className='iframe'  src={"https://www.youtube.com/embed/"+props.id} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
         </>
     )
